80c0b389fe5d995ded622c00e9b63e255a755ce3
Rafael <rvenegas@difvision.com>
2022-11-10 22:31:40 -0500

    add models - prisma

---
+++ prisma/migrations/20221111032927_add_user_drug_and_vaccination_models/migration.sql
@@ -0,0 +1,41 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255),
+    "email" VARCHAR NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Drug" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "approved" BOOLEAN NOT NULL,
+    "min_dose" INTEGER NOT NULL,
+    "max_dose" INTEGER NOT NULL,
+    "available_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Drug_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Vaccination" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "drug_id" INTEGER NOT NULL,
+    "dose" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Vaccination_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Vaccination_drug_id_key" ON "Vaccination"("drug_id");
+
+-- AddForeignKey
+ALTER TABLE "Vaccination" ADD CONSTRAINT "Vaccination_drug_id_fkey" FOREIGN KEY ("drug_id") REFERENCES "Drug"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
