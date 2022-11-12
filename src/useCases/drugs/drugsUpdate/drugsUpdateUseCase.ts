80ed6740e2f790e8db8f8d06c784799dcdd24f7c
Rafael <rvenegas@difvision.com>
2022-11-11 22:56:00 -0500

    update drugsController - add drusUpdateUseCase

---
+++ src/useCases/drugs/drugsUpdate/drugsUpdateUseCase.ts
@@ -0,0 +1,31 @@
+import { DrugsUpdateOptionsInterface } from "./drugsUpdateOptionsInterface";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const drugsUpdateUseCase = async (
+  options: DrugsUpdateOptionsInterface
+) => {
+  const drugDB = await prisma.drug.findUnique({
+    where: { id: options.id },
+  });
+
+  if (!drugDB) {
+    return {
+      type: "does_not_exist",
+      message: `Drug doesn't exist, please check it`,
+    };
+  }
+
+  const updatedDrug = await prisma.drug.update({
+    where: { id: options.id },
+    data: {
+      ...options.data,
+    },
+  });
+
+  return {
+    message: `Drug update successfully`,
+    drug: { ...updatedDrug },
+  };
+};
