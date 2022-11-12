337053fb4a265a8da09ec04252761952203ae953
Rafael <rvenegas@difvision.com>
2022-11-11 21:42:42 -0500

    refactor authController - add status switch

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -7,8 +7,17 @@ export const authController = {
     try {
       const { email, password } = req.body;
       const data = await authLoginUseCase({ email, password });
-
-      res.json({ data });
+      switch (data.type) {
+        case "incomplete_info" || "not_found":
+          res.status(400).json({ data });
+          break;
+        case "not_matched_pass":
+          res.status(409).json({ data });
+          break;
+        default:
+          res.status(201).json({ data });
+          break;
+      }
     } catch (error) {
       next(error);
     }
@@ -18,10 +27,10 @@ export const authController = {
       const { email, password, name } = req.body;
       const data = await authSignupUseCase({ email, password, name });
       switch (data.type) {
-        case "incomplete_info" || "not_found":
+        case "incomplete_info":
           res.status(400).json({ data });
           break;
-        case "not_matched_pass":
+        case "already_exist":
           res.status(409).json({ data });
           break;
         default:
